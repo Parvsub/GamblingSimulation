77975701126ceb69f8c20aa587dab1de688c2b6c
Parvsub <Praveenboda421@gmail.com>
2023-02-12 15:34:52 +0530

    Added: Stack everyday and bet every game

---
+++ GamblingSimulator.java
@@ -0,0 +1,10 @@
+package com.gamblersimulation;
+
+public class GamblingSimulator {
+    public static final int InitialBetStake = 100;
+    public static final int BetAmount = 1;
+    public static void main(String[] args) {
+
+    }
+
+}
