5a681434248caf8ecb4e832a9154b3a039c2fd21
Parvsub <Praveenboda421@gmail.com>
2023-02-12 16:31:09 +0530

    Added:Know the days won and lost and by how much

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,17 +2,18 @@ package com.gamblersimulation;
 
 import java.util.*;
 public class GamblingSimulator {
-    static int AmountOfStake=100;
+    static int AmountOFStake=100;
+    static int TotalAmountDifferenceInMonth=0;
     static int BetAmount=1;
     static int Won=1;
     static int Lost=0;
     static int TotalAmount=0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Starting amount of stake is "+AmountOfStake);
+        System.out.println("Starting amount of stake is "+AmountOFStake);
         System.out.println("Amount to bet after every play "+BetAmount);
         for (int i = 1; i <= 20; i++) {
-            int cashInHand = AmountOfStake;
+            int cashInHand = AmountOFStake;
             while(cashInHand > 50 && cashInHand < 150) {
                 int play = (int) Math.floor(Math.random() * 10) % 2;
                 System.out.println("Gamblers play:"+play);
@@ -29,20 +30,32 @@ public class GamblingSimulator {
                     default:
                         System.out.println("default");
                 }
-                if(play == 1) {
-                    System.out.println("Player won the current play");
-                }
-                else {
-                    System.out.println("Player lost the current play");
-                }
 
             }
             if(cashInHand==150 || cashInHand==50) {
+
                 System.out.println("Player would like to resign for current day"+i);
+            }
+            if(cashInHand > 100) {
+                int amountWonPerDay = cashInHand - AmountOFStake;
+                System.out.println("Player won day"+i+" RS "+amountWonPerDay);
 
             }
+            else if(cashInHand < 100){
+                int amountLostPerDay = AmountOFStake - cashInHand;
+                System.out.println("Player lost day"+i+" RS "+amountLostPerDay);
+
+            }
+
             TotalAmount = TotalAmount + cashInHand;
         }
         System.out.println("Total amount player has after 20 days as per day included = "+TotalAmount+" $");
+        TotalAmountDifferenceInMonth = TotalAmount - AmountOFStake * 20;
+        if(TotalAmountDifferenceInMonth > 0) {
+            System.out.println("Total cash gambler won "+TotalAmountDifferenceInMonth+" $");
+        }
+        else {
+            System.out.println("Total cash gambler lost in that month "+TotalAmountDifferenceInMonth+" $");
+        }
     }
 }
