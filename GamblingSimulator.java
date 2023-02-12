22986cf44a9e31effaa71d655ca39dccb779ea07
Parvsub <Praveenboda421@gmail.com>
2023-02-12 16:01:02 +0530

    Added:If Won or lost 50% of the stack,resign for the day

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,36 +2,42 @@ package com.gamblersimulation;
 
 import java.util.*;
 public class GamblingSimulator {
-    static int AmountOfStake=100;
+    static int AmountOFStake=100;
     static int BetAmount=1;
     static int Won=1;
     static int Lost=0;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Starting amount of stake is "+AmountOfStake);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Starting amount of stake is "+AmountOFStake);
         System.out.println("Amount to bet after every play "+BetAmount);
-        int play = (int) Math.floor(Math.random() * 10) % 2;
-        System.out.println("Gamblers play:"+play);
-        int cashInHand = AmountOfStake;
-        switch(play) {
-            case 1:
-                cashInHand = cashInHand + BetAmount;
-                System.out.println("Total current cash gambler has "+cashInHand);
-                break;
-            case 0:
-                cashInHand = cashInHand - BetAmount;
-                System.out.println("Total current cash gambler has "+cashInHand);
-                break;
-            default:
-                System.out.println("default");
-        }
-        if(play == 1) {
-            System.out.println("Player won the first play");
-        }
-        else {
-            System.out.println("Player lost the first play");
-        }
+        int cashInHand = AmountOFStake;
+        while(cashInHand > 50 && cashInHand < 150) {
+            int play = (int) Math.floor(Math.random() * 10) % 2;
+            System.out.println("Gamblers play:"+play);
 
+            switch(play) {
+                case 1:
+                    cashInHand = cashInHand + BetAmount;
+                    System.out.println("Total current cash gambler has "+cashInHand);
+                    break;
+                case 0:
+                    cashInHand = cashInHand - BetAmount;
+                    System.out.println("Total current cash gambler has "+cashInHand);
+                    break;
+                default:
+                    System.out.println("default");
+            }
+            if(play == 1) {
+                System.out.println("Player won the current play");
+            }
+            else {
+                System.out.println("Player lost the current play");
+            }
+
+        }
+        if(cashInHand==150 || cashInHand==50) {
+            System.out.println("Player would like to resign for current day");
+        }
     }
 
 }
