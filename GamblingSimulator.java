4eb38b4f47000c030e4b965e541aa304f903b54f
Parvsub <Praveenboda421@gmail.com>
2023-02-12 16:06:29 +0530

    Added:Playing continously 20 days amount won or lost

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,42 +2,47 @@ package com.gamblersimulation;
 
 import java.util.*;
 public class GamblingSimulator {
-    static int AmountOFStake=100;
+    static int AmountOfStake=100;
     static int BetAmount=1;
     static int Won=1;
     static int Lost=0;
+    static int TotalAmount=0;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Starting amount of stake is "+AmountOFStake);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Starting amount of stake is "+AmountOfStake);
         System.out.println("Amount to bet after every play "+BetAmount);
-        int cashInHand = AmountOFStake;
-        while(cashInHand > 50 && cashInHand < 150) {
-            int play = (int) Math.floor(Math.random() * 10) % 2;
-            System.out.println("Gamblers play:"+play);
+        for (int i = 1; i <= 20; i++) {
+            int cashInHand = AmountOfStake;
+            while(cashInHand > 50 && cashInHand < 150) {
+                int play = (int) Math.floor(Math.random() * 10) % 2;
+                System.out.println("Gamblers play:"+play);
 
-            switch(play) {
-                case 1:
-                    cashInHand = cashInHand + BetAmount;
-                    System.out.println("Total current cash gambler has "+cashInHand);
-                    break;
-                case 0:
-                    cashInHand = cashInHand - BetAmount;
-                    System.out.println("Total current cash gambler has "+cashInHand);
-                    break;
-                default:
-                    System.out.println("default");
-            }
-            if(play == 1) {
-                System.out.println("Player won the current play");
-            }
-            else {
-                System.out.println("Player lost the current play");
-            }
+                switch(play) {
+                    case 1:
+                        cashInHand = cashInHand + BetAmount;
+                        System.out.println("Total current cash gambler has "+cashInHand);
+                        break;
+                    case 0:
+                        cashInHand = cashInHand - BetAmount;
+                        System.out.println("Total current cash gambler has "+cashInHand);
+                        break;
+                    default:
+                        System.out.println("default");
+                }
+                if(play == 1) {
+                    System.out.println("Player won the current play");
+                }
+                else {
+                    System.out.println("Player lost the current play");
+                }
 
+            }
+            if(cashInHand==150 || cashInHand==50) {
+                System.out.println("Player would like to resign for current day"+i);
+
+            }
+            TotalAmount = TotalAmount + cashInHand;
         }
-        if(cashInHand==150 || cashInHand==50) {
-            System.out.println("Player would like to resign for current day");
-        }
+        System.out.println("Total amount player has after 20 days as per day included = "+TotalAmount+" $");
     }
-
 }
